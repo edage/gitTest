d8cc729e205a53e177348008934f09f449a9474d
mkin <fnsony@163.com>
2022-07-08 22:09:41 +0800

    15 commit

---
+++ src/main/java/com/yzc/gittest/controller/aaa.java
@@ -0,0 +1,13 @@
+package com.yzc.gittest.controller;
+
+/**
+ * @author 知者
+ * @version 1.0
+ * 2022-07-08
+ * Description：
+ **/
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
